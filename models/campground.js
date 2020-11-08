09a3981084b4300256bc49ebe32e41bae315f02a
dhruvbansal49 <43701906+dhruvbansal49@users.noreply.github.com>
2020-11-08 22:40:53 +0530

    Styling

--- models/campground.js
+++ models/campground.js
@@ -3,9 +3,10 @@ const schema=mongoose.Schema;
 
 const CampgroundSchema=new schema({
     title:String,
-    price:String,
+    price:Number,
     description:String,
-    location:String
+    location:String,
+    image:String
 })
 
 module.exports=mongoose.model('Campground',CampgroundSchema);
