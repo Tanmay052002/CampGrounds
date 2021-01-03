bde8825f885cd532fa65de3505987a05d8691c8d
Dhruv <bansaldhruv49@gmail.com>
2021-01-03 21:47:34 +0530

    XSS Handled

--- schema.js
+++ schema.js
@@ -1,17 +1,40 @@
-const joi=require('joi');
+const withoutsanitizejoi=require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML':'HTML should not be present in {#label}!!'
+    },
+    rules: {
+        escapeHTML:{
+            validate(value, helpers){
+                const clean = sanitizeHtml(value, {
+                    allowedTags:[],
+                    allowedAttributes:{},
+                });
+                if(clean!==value) return helpers.error('string.escapeHTML',{value})
+                return clean;
+            }
+        }
+    }
+})
+
+const joi = withoutsanitizejoi.extend(extension); 
+
 module.exports.campSchema=joi.object({
     campground:joi.object({
-        title:joi.string().required(),
+        title:joi.string().required().escapeHTML(),
         price:joi.number().required().min(0),
-        // image:joi.string().required(),
-        location:joi.string().required(),
-        description:joi.string().required()
+        location:joi.string().required().escapeHTML(),
+        description:joi.string().required().escapeHTML()
     }).required(),
     deleteImages:joi.array()
 })
 module.exports.reviewSchema=joi.object({
     review:joi.object({
         rating:joi.number().required().min(1).max(5),
-        body:joi.string().required()
+        body:joi.string().required().escapeHTML()
     }).required()
 })
